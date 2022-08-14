97fdb70fcbd6f050eb482475bd9bd7144a203c70
VishwajeethV <spacexblue80@gmail.com>
2022-08-14 02:03:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -43,8 +43,7 @@ function draw() {
 
 image(video,0,0,300,250);
 fill(130,12,0);
-circle(nose_x,nose_y,2);
-image(mask,nose_x-15,nose_y-15,50,50);
+image(mask,nose_x,nose_y,80,80);
 }
 
 function take_snapshot() {
